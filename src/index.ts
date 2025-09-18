9f40ab2ea57bfdd35b75ac9fb5efb7caa91beb25
RyannKim327 <rksesgundo123@gmail.com>
2025-09-18 19:37:00 +0800

    09-18-25 19:36

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,28 @@ const app = express()
 const PORT = 3000
 app.use(express.json())
 
+const candidate_test = {
+    "male": [
+        'candidate_1',
+        'candidate_2'
+    ],
+    "female": [
+        'candidate_1',
+        'candidate_2'
+    ]
+}
+
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/template/index.html`)
+})
+
+app.get("/candidates", (req, res) => {
+    if(req.query['code'] == "hellolord"){
+        return res.send(candidate_test)
+    }
+    return res.send({
+        "error": "Not authorized"
+    })
 })
 
 app.listen(PORT, () => {
