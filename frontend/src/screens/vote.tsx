abd195d7094d4de61512561d98b5d64aa3dda989
RyannKim327 <rksesgundo123@gmail.com>
2025-09-23 14:05:01 +0800

    09-23-25 14:04

--- frontend/src/screens/vote.tsx
+++ frontend/src/screens/vote.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
+import { useForm, type FieldValues, type UseFormHandleSubmit } from "react-hook-form";
 import Header from "../components/header";
 import type { candidate, participant } from "../interfaces";
 import axios from "axios";
+import { useFormState } from "react-dom";
 
 export default function Vote(){
     const [contestants, setContestants] = useState<candidate>()
     const [message, setMessage] = useState("")
+    const {register, handleSubmit, formState: {errors}} = useForm()
+
     useEffect(() => {
         (async () => {
             try{
@@ -17,34 +21,51 @@ export default function Vote(){
         })()
     }, [])
 
+    const submit = (data: UseFormHandleSubmit<FieldValues, FieldValues>) => {
+        setMessage("")
+    }
+
+    const numberValidator = (input: React.ChangeEvent<HTMLInputElement>) => {
+        input.target.value = input.target.value.replace(/[^0-9.]/g, "");
+		if ((input.target.value.match(/\./g) || []).length > 1) {
+			input.target.value = input.target.value.slice(0, -1);
+		}
+    }
+
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
             <Header/>
             {message}
-            <div className="flex flex-row">
-                <div className="flex flex-col w-full">
-                    {contestants?.male?.map((contestant: participant) => {
-                        return (
-                            <div>
-                                <img src={`http://localhost:3000/assets/male/candidate_${contestant.number}.png`} />
-                                <h3>{contestant.department}</h3>
-                            </div>
-                        )
-                    })}
+            <form onSubmit={handleSubmit(submit)} className="w-full flex flex-col gap-1">
+                <div className="flex flex-row">
+                    <div className="flex flex-col w-full gap-1">
+                        {contestants?.male?.map((contestant: participant) => {
+                            return (
+                                <div>
+                                    <img src={`http://localhost:3000/assets/male/candidate_${contestant.number}.png`} />
+                                    <div>
+                                        <h3>{contestant.department}</h3>
+                                        <input type="" onChange={numberValidator} />
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="flex flex-col w-full gap-1">
+                        {contestants?.female?.map((contestant: participant) => {
+                            return (
+                                <div className="flex flex-row">
+                                    <img className="aspect-[9_16] w-[10%]" src={`http://localhost:3000/assets/female/candidate_${contestant.number}.png`} />
+                                    <span>
+                                        <h3>{contestant.department}</h3>
+                                    </span>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-                <div className="flex flex-col w-full">
-                    {contestants?.female?.map((contestant: participant) => {
-                        return (
-                            <div className="flex flex-row">
-                                <img className="aspect-[9_16] w-[10%]" src={`http://localhost:3000/assets/female/candidate_${contestant.number}.png`} />
-                                <span>
-                                    <h3>{contestant.department}</h3>
-                                </span>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+                <input type="submit" className="" />
+            </form>
         </div>
     )
 }
