f78552efc555b36baf15ac64d9f9e05321c4fa66
RyannKim327 <rksesgundo123@gmail.com>
2025-10-02 18:41:35 +0800

    10-02-25 18:41

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,91 @@
 import Vote from './screens/vote'
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; 
+import type { Container, Engine } from '@tsparticles/engine';
 
 function App() {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine: Engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const loaded = (container: Container) => {
+    console.log(container);
+  };
+
+  const opts = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "transparent",
+        },
+      },
+      fpsLimit: 120,
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
 
   return (
-    <div className='bg-[url(/background.jpg)] bg-no-repeat bg-cover bg-fixed w-dvw h-dvh p-2'>
-      <Vote />
+    <div className='relative bg-slate-800 w-dvw h-dvh'>
+      {init ? 
+        <Particles 
+          id="tsparticles"
+          options={opts}
+          particlesLoaded={loaded}
+        /> : null
+      }
+      <div className='relative top-0 z-10 w-full h-full p-2'>
+        <Vote />
+      </div>
     </div>
   )
 }
